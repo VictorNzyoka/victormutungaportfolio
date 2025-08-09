9769b396238a8caca066aa5fa854be302225b239
VictorNzyoka <mutunga.victor21@students.dkut.ac.ke>
2025-08-09 16:57:29 +0300

    Deployment errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,23 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-  return (
-    <input
-      ref={ref}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-neutral-800 bg-neutral-950 px-3 text-sm text-neutral-100",
-        "placeholder:text-neutral-600 focus:outline-none focus:ring-2 focus:ring-emerald-500",
-        "disabled:cursor-not-allowed disabled:opacity-50",
-        className,
-      )}
-      {...props}
-    />
-  )
-})
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-neutral-800 bg-neutral-950 px-3 text-sm text-neutral-100",
+          "placeholder:text-neutral-600 focus:outline-none focus:ring-2 focus:ring-emerald-500",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      />
+    )
+  }
+)
 Input.displayName = "Input"
